9be5728a0a542137834599b0ac8ddd6f6a119ba8
Samuel Pomeroy <1690@holbertonschool.com>
2023-08-26 03:09:53 +0000

    rm file redundant with Catch2 v2.13.10 includes/catch_with_main.hpp

--- tests/catch_main.cpp
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
-
-// Placing this define and include into a separate translation unit aids in
-//   reducing compilation time of tests, see:
-//   - https://github.com/catchorg/Catch2/blob/v2.x/docs/slow-compiles.md
